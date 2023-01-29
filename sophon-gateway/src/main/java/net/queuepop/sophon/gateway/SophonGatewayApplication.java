04716e36bc2b05cb890679e11ad0e91dea7d18ef
wangchong <wangchong@samsundot.com>
2023-01-29 11:01:19 +0800

    1

---
+++ sophon-gateway/src/main/java/net/queuepop/sophon/gateway/SophonGatewayApplication.java
@@ -0,0 +1,13 @@
+package net.queuepop.sophon.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SophonGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SophonGatewayApplication.class, args);
+    }
+
+}
---
+++ sophon-gateway/src/test/java/net/queuepop/sophon/gateway/SophonGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package net.queuepop.sophon.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SophonGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
